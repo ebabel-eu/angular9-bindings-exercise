ad9449897ad0ccb2c8354955dcad1f9a0507e4e2
Thomas Amar <hello@ebabel.eu>
2020-04-28 06:29:24 +0200

    Add header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
 import { EvenComponent } from './even/even.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameControlComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
