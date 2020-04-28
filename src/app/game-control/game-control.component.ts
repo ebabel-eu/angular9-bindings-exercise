029f0f1eedf45082e35c3126a4cfd86b6deb6b3b
Thomas Amar <hello@ebabel.eu>
2020-04-28 07:02:31 +0200

    Start and stop the game.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  randomNumber: number;
+  @Output() numberUpdated = new EventEmitter<number>();
+  timerId: NodeJS.Timeout = null;
+  speed: number = 1000;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  startGame() {
+    this.randomNumber = Math.floor(Math.random() * 999);
+    this.numberUpdated.emit(this.randomNumber);  
+
+    this.timerId = setInterval(() => {
+      this.randomNumber = Math.floor(Math.random() * 999);
+      this.numberUpdated.emit(this.randomNumber);  
+    }, this.speed);
+  }
+
+  stopGame() {
+    clearInterval(this.timerId);
+  }
 }
